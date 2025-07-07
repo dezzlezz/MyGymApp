beb9c17cef90e6c6bd340922c41f7233b429d0bc
dezzlezz <by.dezzlezz@gmail.com>
2025-07-07 15:35:22 +0200

    d

--- app/src/main/java/com/example/mygymapp/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/mygymapp/ui/screens/ProfileScreen.kt
@@ -1,18 +1,9 @@
 package com.example.mygymapp.ui.screens
 
-import androidx.compose.material3.*
-import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+
 
 @Composable
 fun ProfileScreen() {
-    Box(
-        Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text("[ScreenName] Screen", style = MaterialTheme.typography.headlineMedium)
-    }
+    RainyForestScreen()
 }
