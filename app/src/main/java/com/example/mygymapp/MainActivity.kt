8a22adb64cb25593cbdddcf5b80d7e51de034313
dezzlezz <154933255+dezzlezz@users.noreply.github.com>
2025-07-10 16:20:24 +0200

    Fix navHost modifier

--- app/src/main/java/com/example/mygymapp/MainActivity.kt
+++ app/src/main/java/com/example/mygymapp/MainActivity.kt
@@ -5,19 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import com.example.mygymapp.ui.MainScreen
-import com.example.mygymapp.ui.theme.MyGymAppTheme
-import com.example.mygymapp.data.SettingsStorage
+import com.example.mygymapp.ui.theme.MyGymAppThemeWrapper
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mygymapp.viewmodel.ThemeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val settings = SettingsStorage.getInstance(this)
         setContent {
-            val dark by settings.darkMode.collectAsState()
-            MyGymAppTheme(darkTheme = dark) {
-                MainScreen()
+            val themeVm: ThemeViewModel = viewModel()
+            val theme by themeVm.currentTheme.collectAsState()
+            MyGymAppThemeWrapper(theme) {
+                MainScreen(themeVm)
             }
         }
     }
