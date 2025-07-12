c8d998c304b7c954ff1af6f7b44dc1aa06cfab51
dezzlezz <154933255+dezzlezz@users.noreply.github.com>
2025-07-12 20:42:54 +0200

    feat: enhance exercises tab with filters

--- app/src/main/java/com/example/mygymapp/data/AppDatabase.kt
+++ app/src/main/java/com/example/mygymapp/data/AppDatabase.kt
@@ -14,7 +14,7 @@ import com.example.mygymapp.data.PlanDay
         Exercise::class,
         PlanDay::class
     ],
-    version = 6,
+    version = 7,
     exportSchema = false
 )
 @TypeConverters(PlanConverters::class)
