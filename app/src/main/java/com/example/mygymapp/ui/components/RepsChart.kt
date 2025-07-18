53b5bbdb61caf18370efad7776273887070ad5bb
dezzlezz <by.dezzlezz@gmail.com>
2025-07-18 09:38:30 +0200

    Color.kt, MainScreen.kt, MyGymAppTheme.kt changed

--- app/src/main/java/com/example/mygymapp/ui/components/RepsChart.kt
+++ app/src/main/java/com/example/mygymapp/ui/components/RepsChart.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import com.example.mygymapp.model.ExerciseLogEntry
+import androidx.compose.foundation.layout.height
 
 @Composable
 /**
