768b69d7f5574fd46f51520af3f1a07b5b12b8ef
dezzlezz <by.dezzlezz@gmail.com>
2025-07-20 10:25:59 +0200

    Color.kt, MainScreen.kt, MyGymAppTheme.kt changed

--- app/src/main/java/com/example/mygymapp/MainActivity.kt
+++ app/src/main/java/com/example/mygymapp/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.mygymapp.navigation.AppNavHost
+import com.example.mygymapp.ui.screens.MainScreen
 import com.example.mygymapp.ui.theme.MyGymAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             MyGymAppTheme {
                 MainScreen()
             }
