40d032c94c81951a7b18dbfd481a59f700049b7a
dezzlezz <154933255+dezzlezz@users.noreply.github.com>
2025-07-22 14:49:19 +0200

    Remove old screens and navigation

--- app/src/main/java/com/example/mygymapp/ui/components/AddEditPlanSheet.kt
+++ app/src/main/java/com/example/mygymapp/ui/components/AddEditPlanSheet.kt
@@ -1,4 +1,4 @@
-package com.example.mygymapp.ui.screens
+package com.example.mygymapp.ui.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
