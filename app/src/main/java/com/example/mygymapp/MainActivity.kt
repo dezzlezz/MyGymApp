db0e3ff43a62280d939882de0a9fb0b8aa3d369f
dezzlezz <154933255+dezzlezz@users.noreply.github.com>
2025-07-22 22:46:10 +0200

    Improve bookmark menu design

--- app/src/main/java/com/example/mygymapp/MainActivity.kt
+++ app/src/main/java/com/example/mygymapp/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.mygymapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.example.mygymapp.ui.theme.MyGymAppTheme
 import com.example.mygymapp.ui.pages.PageScaffold
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             MyGymAppTheme {
                 PageScaffold()
